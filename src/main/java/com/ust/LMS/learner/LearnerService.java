3e9a95239f0335cb49d12ef23ff05f23258710e3
unknown <jeswinsam.sundar@gmail.com>
2025-04-29 11:53:27 +0530

    added automatic email when user is created

--- src/main/java/com/ust/LMS/learner/LearnerService.java
+++ src/main/java/com/ust/LMS/learner/LearnerService.java
@@ -1,15 +1,20 @@
 package com.ust.LMS.learner;
 
-import com.ust.LMS.batch.Batch;
 import com.ust.LMS.batch.BatchRepository;
 import com.ust.LMS.course.Course;
 import com.ust.LMS.course.CourseDTO;
 import com.ust.LMS.course.CourseRepository;
+import com.ust.LMS.security.AppUser;
+import com.ust.LMS.security.AppUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 @Service
@@ -23,6 +28,12 @@ public class LearnerService {
     private CourseRepository courseRepository;
     @Autowired
     private LearnerMapper learnerMapper;
+    @Autowired
+    private AppUserRepository appUserRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private JavaMailSender mailSender;
 
     public List<LearnerDTO> getAllLearners() {
         return learnerRepository.findAll().stream()
@@ -37,16 +48,52 @@ public class LearnerService {
     }
 
     public LearnerDTO saveLearner(LearnerDTO dto) {
-        Batch batch = batchRepository.findById(dto.getBatchId()).orElse(null);
-        List<Course> courses = dto.getCourseIds() != null ?
-                courseRepository.findAllById(dto.getCourseIds()) : null;
+        Learner learner = new Learner();
+        learner.setName(dto.getName());
+        learner.setEmail(dto.getEmail());
+        learnerRepository.save(learner);
 
-        Learner learner = learnerMapper.toEntity(dto);
-        learner.setBatch(batch);
-        learner.setCourses(courses);
+        // Generate random password
+        String randomPassword = generateRandomPassword();
+
+        // Create AppUser
+        AppUser user = new AppUser();
+        user.setEmail(dto.getEmail());
+        user.setPassword(passwordEncoder.encode(randomPassword));
+        user.setRole("LEARNER");
+        appUserRepository.save(user);
+
+        // Send welcome email
+        sendWelcomeEmail(dto.getEmail(), dto.getName(), randomPassword);
 
         return learnerMapper.toDTO(learnerRepository.save(learner));
     }
+
+    private void sendWelcomeEmail(String email, String name, String password) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(email);
+        message.setSubject("Welcome to LMS - Login Details");
+        message.setText("Dear " + name + ",\n\n"
+                + "Your LMS account has been created.\n"
+                + "Here are your login credentials:\n\n"
+                + "Email: " + email + "\n"
+                + "Password: " + password + "\n\n"
+                + "Please login and change your password.\n\n"
+                + "Regards,\nLMS Team");
+        mailSender.send(message);
+    }
+
+    private String generateRandomPassword() {
+        int length = 8;
+        String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#&!";
+        Random random = new Random();
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            sb.append(chars.charAt(random.nextInt(chars.length())));
+        }
+        return sb.toString();
+    }
+
 
     public List<CourseDTO> getCoursesForLearner(String email) {
         Learner learner = learnerRepository.findByEmail(email)
--- src/main/java/com/ust/LMS/trainer/TrainerService.java
+++ src/main/java/com/ust/LMS/trainer/TrainerService.java
@@ -1,16 +1,29 @@
 package com.ust.LMS.trainer;
 
+import com.ust.LMS.security.AppUser;
+import com.ust.LMS.security.AppUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 @Service
 public class TrainerService {
-    @Autowired private TrainerRepository repo;
+    @Autowired
+    private TrainerRepository repo;
     @Autowired
     private TrainerMapper mapper;
+    @Autowired
+    private AppUserRepository appUserRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private JavaMailSender mailSender;
 
     public List<TrainerDTO> getAll() {
         return repo.findAll().stream().map(mapper::toDTO).collect(Collectors.toList());
@@ -21,17 +34,50 @@ public class TrainerService {
     }
 
     public TrainerDTO save(TrainerDTO dto) {
-        Trainer trainer = mapper.toEntity(dto);
+        Trainer trainer = new Trainer();
+        trainer.setName(dto.getName());
+        trainer.setEmail(dto.getEmail());
+        repo.save(trainer);
 
-        // Set availability based on batch assignment
-        if (dto.getBatchId() != null) {
-            trainer.setIsAvailable(false);
-        } else {
-            trainer.setIsAvailable(true);
-        }
+        // Generate random password
+        String randomPassword = generateRandomPassword();
 
-        // Save and return DTO
+        // Create AppUser
+        AppUser user = new AppUser();
+        user.setEmail(dto.getEmail());
+        user.setPassword(passwordEncoder.encode(randomPassword));
+        user.setRole("TRAINER");
+        appUserRepository.save(user);
+
+        // Send welcome email
+        sendWelcomeEmail(dto.getEmail(), dto.getName(), randomPassword);
+
         return mapper.toDTO(repo.save(trainer));
+    }
+
+    private void sendWelcomeEmail(String email, String name, String password) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(email);
+        message.setSubject("Welcome to LMS - Login Details");
+        message.setText("Dear " + name + ",\n\n"
+                + "Your LMS account has been created.\n"
+                + "Here are your login credentials:\n\n"
+                + "Email: " + email + "\n"
+                + "Password: " + password + "\n\n"
+                + "Please login and change your password.\n\n"
+                + "Regards,\nLMS Team");
+        mailSender.send(message);
+    }
+
+    private String generateRandomPassword() {
+        int length = 8;
+        String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#&!";
+        Random random = new Random();
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            sb.append(chars.charAt(random.nextInt(chars.length())));
+        }
+        return sb.toString();
     }
 
 
